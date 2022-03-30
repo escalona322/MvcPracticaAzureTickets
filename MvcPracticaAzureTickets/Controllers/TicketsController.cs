19690d2686604353cb9adf3c5ae80f8015daa1be
Jose Antonio Lozano <escalona322@gmail.com>
2022-03-30 11:38:05 +0200

    Add project files.

---
+++ MvcPracticaAzureTickets/Controllers/TicketsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcPracticaAzureTickets.Models;
+using MvcPracticaAzureTickets.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPracticaAzureTickets.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private ServiceApiTickets service;
+        private ServiceStorageBlobs serviceBlobs;
+
+        public TicketsController(ServiceApiTickets service, ServiceStorageBlobs serviceBlobs)
+        {
+            this.service = service;
+            this.serviceBlobs = serviceBlobs;
+        }
+        public IActionResult CrearUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearUsuario(Usuario usu)
+        {
+            await this.service.InsertUsuario(usu);
+            return View();
+        }
+
+        public IActionResult CrearTicket()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearTicket(int idticket, int idusuario, DateTime fecha
+            , string importe, string producto, IFormFile file)
+        {
+            string containerName = "tickets";
+            string blobName = file.FileName;
+            using (Stream stream = file.OpenReadStream())
+            {
+                await this.serviceBlobs.UploadBlobAsync(containerName, blobName, stream);
+            }
+
+            Ticket ticket = new Ticket
+            {
+                IdTicket = idticket,
+                IdUsuario = idusuario,
+                Fecha = fecha,
+                Importe = importe,
+                Producto = producto,
+                FileName = blobName,
+                StoragePath = "https://storagetajamarjalt.blob.core.windows.net/tickets/" + blobName
+            };
+            //List<BlobClass> blobs = await this.serviceBlobs.GetBlobsAsync(containerName);
+
+            //foreach(BlobClass blob in blobs)
+            //{
+            //    if(blob.Nombre.Contains(blobName))
+            //    {
+            //        ticket.StoragePath = blob.Url;
+            //    }
+            //}
+
+            await this.service.InsertTicket(ticket);
+
+            return View();
+        }
+
+        public async Task<IActionResult> TicketsUsuario(int idusuario)
+        {
+            List<Ticket> tickets = await this.service.GetTicketsUsuarioAsync(idusuario);
+
+            return View(tickets);
+        }
+
+    }
+}
---
+++ MvcPracticaAzureTickets/Services/ServiceApiTickets.cs
@@ -0,0 +1,119 @@
+﻿using MvcPracticaAzureTickets.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcPracticaAzureTickets.Services
+{
+    public class ServiceApiTickets
+    {
+        private Uri UriApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceApiTickets(string url)
+        {
+            this.UriApi = new Uri(url);
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    T data =
+                        await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Ticket>> GetTicketsUsuarioAsync(int idusuario)
+        {
+            string request = "/api/Ticket/TicketUsuario/" + idusuario;
+            List<Ticket> tickets =
+                await this.CallApiAsync<List<Ticket>>(request);
+
+            return tickets;
+        }
+
+        public async Task<Ticket> FindTicket(int idticket)
+        {
+            string request = "/api/Ticket/FindTicket/" + idticket;
+            Ticket tickets =
+                await this.CallApiAsync<Ticket>(request);
+
+            return tickets;
+        }
+
+        public async Task InsertUsuario(Usuario usu)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/Ticket/InsertUsuario";
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+  
+                string json = JsonConvert.SerializeObject(usu);
+
+                StringContent content = new StringContent
+                    (json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response =
+                    await client.PostAsync(request, content);
+            }
+        }
+
+        public async Task InsertTicket(Ticket ticket)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/Ticket/InsertTicket";
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+ 
+                string json = JsonConvert.SerializeObject(ticket);
+
+                StringContent content = new StringContent
+                    (json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response =
+                    await client.PostAsync(request, content);
+            }
+        }
+
+        public async Task ProcesarTicket(Ticket ticket)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/Ticket/ProcesarTicket";
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                string json = JsonConvert.SerializeObject(ticket);
+
+                StringContent content = new StringContent
+                    (json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response =
+                    await client.PostAsync(request, content);
+            }
+        }
+    }
+}
